d2307ffa1ee86fb1da5f791a0d19d1eab0e47f45
Coder <tcssanramon@gmail.com>
2022-10-15 13:58:10 -0700

    Actually starting project

--- Src/main.py
+++ Src/main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+waterlily = 5
+roseseed = 5
+gingerroot = 5
+
+# Start of story
+print("You are a novice Witch, who is going to start her own shop.")
+print("You find an abandoned cottage, which you decided to turn into a shop.")
+print("You find a cauldron and some notes off recipies.")
+print("You are now going to start your journey.")
+
+answer = input("What would you like to do? ").lower()
+if answer == "potion":
+    print("You move to the cauldron, what would you like to add?")
