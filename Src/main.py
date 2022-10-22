ef5a4ed108f15f049ed8b8a15a7d11f06c5734c5
TCS SanRamon <a.j.willburn@gmail.com>
2022-10-22 13:54:59 -0700

    Finish Potion Ingredient System

--- Src/main.py
+++ Src/main.py
@@ -1,6 +1,8 @@
 waterlily = 5
-roseseed = 5
+rosepetal = 5
 gingerroot = 5
+ingredlist = {"water lily":1,"rose petal":3,"ginger root":5}
+potionlist = {9:"Herbal Medicine"}
 
 # Start of story
 print("You are a novice Witch, who is going to start her own shop.")
@@ -10,4 +12,15 @@ print("You are now going to start your journey.")
 
 answer = input("What would you like to do? ").lower()
 if answer == "potion":
-    print("You move to the cauldron, what would you like to add?")
+    print("You move to the cauldron, you then add the water. (Type done when you want to exit)")
+    potion = 0
+    ingred = ""
+    while ingred != "done":
+        ingred = input("What else would you like to add?\n")
+        if ingred in ingredlist:
+            potion += ingredlist[ingred]
+        elif ingred != "done":
+            print("You do not have that ingredient.")
+        print(potion)
+    print("You heat the potion, stir it, then wait for the results.)
+          
