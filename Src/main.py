28ed51b7631661311b1f2b1d4b51a252ca434a80
theCoderSchool San Ramon <thecoderschoolsanramon@theCoderSchools-iMac-2.local>
2022-11-19 13:59:14 -0800

    Finished potion making, starting adventuring and inventory

--- Src/main.py
+++ Src/main.py
@@ -3,10 +3,18 @@ rosepetal = 5
 gingerroot = 5
 ingredlist = {"water lily":1,"rose petal":3,"ginger root":5}
 potionlist = {9:"Herbal Medicine"}
+mapplaces = ["Thornwood"]
+inventory = [""]
+
+def idenpotion(p):
+    for key,value in potionlist.items():
+        if p == key:
+            return value
+    return "Rainbow Sludge"
 
 # Start of story
 print("You are a novice Witch, who is going to start her own shop.")
-print("You find an abandoned cottage, which you decided to turn into a shop.")
+print("You find an abandoned` cottage, which you decided to turn into a shop.")
 print("You find a cauldron and some notes off recipies.")
 print("You are now going to start your journey.")
 
@@ -22,5 +30,21 @@ if answer == "potion":
         elif ingred != "done":
             print("You do not have that ingredient.")
         print(potion)
-    print("You heat the potion, stir it, then wait for the results.)
-          
+    if potion != 0:
+        print("You heat the potion, stir it, then wait for the results.")
+        print(idenpotion(potion))
+    else:
+        print("You decide not to brew and leave.")
+if answer == "adventure":
+    print("You decide to go on an adventure, you pull out your map and look where you want to go.")
+    print("You can goto:")
+    for plce in mapplaces:
+        print(plce)
+    print("Where would you like to go?")
+    answer = input()
+    if answer == "Thornwood":
+        print("What potion would you like to bring?")
+        answer = input()
+        if answer == "Herbal Medicine" and answer in inventory:
+            print("You enter the thornwood forest, use your potion, and return ")
+        elif answer not in inventory:
